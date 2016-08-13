f9b6897c2321a72837cd3fd4a1010f0ca4b804cd
dashguys <2392628563@qq.com>
2016-08-13 22:25:23 +0800

    引入 weui

--- font-end/src/pages/me/index.jsx
+++ font-end/src/pages/me/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Page  from '../menu.page';
 import { observer } from 'mobx-react';
-import HomeData from '../../stores/home.model';
-import Banner from '../../components/banner';
+
+import Dialog from '../../components/dialog';
+import Loading from '../../components/loading';
+import ToastDone from '../../components/toast.done';
 import  appState from '../../stores/app.state';
 
 
@@ -12,14 +14,17 @@ class Me extends Page {
     constructor(props) {
         super(props);
         this.displayName = 'Me';
+        this.state = {dialogOpt:{}};
        
     }
     render() {
         return <div className="home-page">
-        	
+        	<Dialog cb={::this.handleDialog} cancelBtn={true} />
         	<div className="app-body">
-                
-              
+            <button className='weui_btn weui_btn_default' onClick={::this.openToast}>show</button>    
+            <i className="weui_icon_success_circle" style={{color:'red'}} ></i>
+            <Loading  />
+            <ToastDone />
             </div>
        
         </div>;
@@ -28,6 +33,14 @@ class Me extends Page {
         appState.topBar.title = '我的';
         appState.menu.currentIndex = 3;
        
+    }
+   
+    handleDialog(flag) {
+       console.log(flag?'ok':'cancel');
+    }
+    openToast() {
+        appState.dialog.showFlag = true;
+        setTimeout( () => { appState.loading.showFlag = false} ,3000)
     }
 }
 
--- font-end/src/stores/app.state.js
+++ font-end/src/stores/app.state.js
@@ -15,13 +15,31 @@ class AppState {
 		}
 
 
-	}
+	};
 	topBar = {
 		@observable title : '大湿',
 		@observable backBtn : false,
 		@observable rightText : '',
 		@observable showFlag : true
+	};
+		toast = {
+		@observable showFlag: false,
+		text: '已完成',
+		triger(){
+			this.showFlag = true;
+			setTimeout(() =>{this.showFlag = false},1000)
+		}
+	};
+	loading = {
+		@observable showFlag: false,
+		text: '数据正在加载中...'
+	};
+	dialog = {
+		@observable showFlag: false,
+		title: '对话标题',
+		content: '测试对话框的内容',
 	}
+
 	constructor(){
 		this.menu.init();
 	}
